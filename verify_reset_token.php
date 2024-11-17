5a16f5364392f1fd06211ec5deca46d6ad8992d8
Rens Saladaga <staleexam19@gmail.com>
2024-11-17 18:46:19 +0800

    chore: reconfig

--- verify_reset_token.php
+++ verify_reset_token.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $servername = "127.0.0.1";
-$username = "root";
-$password = "";
+$username = "Kenji";
+$password = "JamesRyan";
 
 $conn = mysqli_connect($servername, $username, $password, "violation_tracker");
 
